f0b2a6475f353e7bc1993d11909ef4dcc4a124be
Marckhz <marcohdes94i@gmail.com>
2017-06-28 23:07:25 -0600

    Working on bot

---
+++ bot.js
@@ -0,0 +1,49 @@
+console.log("bot is online");;
+
+var Twit = require('twit');
+var config = require('./config');
+var T = new Twit(config);
+var stream = T.stream('user');
+
+stream.on('follow',  followed);
+function followed(eventFollow) {
+
+  var name = eventFollow.source.name;
+  var screenName  = eventFollow.source.screen_name;
+  tweetIt('@' + screenName  + ' ' + 'did you get the message');
+}
+
+
+stream.on('tweet', tweetEvent);
+function tweetEvent(eventMsg){
+
+  var replyto = eventMsg.in_reply_to_screen_name;
+  var text = eventMsg.text;
+  var from = eventMsg.user.screen_name;
+
+  console.log(replyto + ' ' + from);
+
+  if(replyto === 'Coffe_Pot_slave'){
+    var newTweet = '@' + from + 'i am testing';
+    tweetIt(newTweet);
+  }
+}
+
+function tweetIt(txt){
+
+  var tweet =  {
+       status: txt
+      }
+
+      T.post('statuses/update', tweet, tweeted);
+
+      function tweeted (err, data, response) {
+        if (err){
+          console.log("something went wrong");
+        }
+        else{
+          console.log("working like a charm");
+        }
+
+      }
+}
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports  = {
+  consumer_key :"vZBO9YGsE8q5UaNFJeFirRnAC",
+  consumer_secret: "siRXwJaGiAalJyElAvk0HMfGFgwv6ZhfDr6lUz5Vo7oa7K0U9F",
+  access_token : "877976118745116673-cK2KP3GTs4zz37TS87sgSaGVx8E4HLQ",
+  access_token_secret : "o1vwKNjxPTKEIyLcnuWO0SVcFoM7U0AyfCCz8A13ETWnN",
+}
